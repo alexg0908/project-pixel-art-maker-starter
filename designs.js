4717433bdcd77099e2b3b74796698306bc3057de
Alejandro Gamero <alex.gamero@agluxsolutions.com>
2021-04-19 09:29:51 +0200

    Add submit function

--- designs.js
+++ designs.js
@@ -1,7 +1,16 @@
-// Select color input
-// Select size input
 var color;
 var height, width;
+
+// Jquery submit function
+// Select color input
+// Select size input
+
+$('#sizePicker').submit(function (event) {
+  event.preventDefault();
+  height = $('#inputHeight').val();
+  width = $('#inputWidth').val();
+  makeGrid(height, width);
+})
 
 // When size is submitted by the user, call makeGrid()
 
